ff0a41d49908861a1c0fad419a9bcbf71855e78a
Kent C. Dodds <kent@doddsfamily.us>
2015-09-17 13:54:40 -0600

    Renaming files and moving things around for sanity

---
+++ js/directives/todo-escape.test.js
@@ -0,0 +1,33 @@
+describe('todo-escape', function () {
+  var ESCAPE_KEY = 27;
+  var OTHER_KEY = 32;
+  var template = '<input todo-escape="escapePressed=true" />';
+  var scope, compile;
+
+  beforeEach(module('todomvc'));
+
+  beforeEach(inject(function ($rootScope, $compile) {
+    scope = $rootScope.$new();
+    compile = $compile;
+    scope.escapePressed = false;
+  }));
+
+  it('should evaluate the given statement when escape key pressed', function () {
+    var el = compile(template)(scope);
+    el.triggerHandler({type: 'keydown', keyCode: ESCAPE_KEY});
+    expect(scope.escapePressed).toBe(true);
+  });
+
+  it('should evaluate the given statement when escape key pressed', function () {
+    var el = compile(template)(scope);
+    el.triggerHandler({type: 'keydown', keyCode: OTHER_KEY});
+    expect(scope.escapePressed).toBe(false);
+  });
+
+  it('should unbind the handler when the scope is destroyed', function () {
+    var el = compile(template)(scope);
+    scope.$destroy();
+    el.triggerHandler({type: 'keydown', keyCode: ESCAPE_KEY});
+    expect(scope.escapePressed).toBe(false);
+  });
+});
--- karma.conf.js
+++ karma.conf.js
@@ -2,14 +2,13 @@ module.exports = function (config) {
 	'use strict';
 
 	config.set({
-		basePath: '../../',
+		basePath: './',
 		frameworks: ['jasmine'],
 		files: [
 			'node_modules/angular/angular.js',
 			'node_modules/angular-route/angular-route.js',
 			'node_modules/angular-mocks/angular-mocks.js',
-			'js/**/*.js',
-			'test/unit/**/*.js'
+			'js/**/*.js'
 		],
 		autoWatch: true,
 		singleRun: false,
